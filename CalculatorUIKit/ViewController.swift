fbb2c7b4d84ff4f23316a4297fc07afef5ced063
Abouzar <abouzarmoradian1@gmail.com>
2024-09-01 22:38:29 -0400

    added some bussiness logic / improve UI

--- CalculatorUIKit/ViewController.swift
+++ CalculatorUIKit/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    var numberOnScreen: Double = 0
+    var previousNumber: Double = 0
+    var performingMath: Bool = false
+    var operation = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = String(0)
+        
     }
 
-
+    @IBAction func buttons(_ sender: UIButton) {
+        
+        
+    }
+    @IBAction func numbers(_ sender: UIButton) {
+        
+        if performingMath{
+            
+            label.text = String(sender.tag - 1)
+            numberOnScreen = Double(sender.tag - 1)
+            performingMath = false
+        }else{
+            
+            if let currentText = label.text{
+                label.text = currentText + String(sender.tag - 1)
+                numberOnScreen = Double(label.text!)!
+            }
+        }
+    }
+    
 }
 
