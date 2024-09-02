f4f96d030bf3abfdfa4110d03e8233cbb517bce6
Abouzar <abouzarmoradian1@gmail.com>
2024-09-02 14:57:52 -0400

    completed the project

--- CalculatorUIKit/ViewController.swift
+++ CalculatorUIKit/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var label: UILabel!
     
     var numberOnScreen: Double = 0
@@ -21,8 +21,85 @@ class ViewController: UIViewController {
         label.text = String(0)
         
     }
-
+    
     @IBAction func buttons(_ sender: UIButton) {
+        
+        if label.text != "" && sender.tag != 11 && sender.tag != 16{
+            
+            previousNumber = Double(label.text!)!
+            
+            if sender.tag == 12 { //Divide
+                
+                label.text = "/"
+                
+            }
+            
+            if sender.tag == 13 { //Multiply
+                
+                label.text = "x"
+                
+            }
+            
+            if sender.tag == 14 { //Subtract
+                
+                label.text = "-"
+                
+            }
+            
+            if sender.tag == 15 { //Add
+                
+                label.text = "+"
+                
+            }
+            
+            operation = sender.tag
+            
+            performingMath = true
+            
+        }
+        
+        else if sender.tag == 16 {
+            
+            if operation == 12{ //Divide
+                
+                label.text = String(previousNumber / numberOnScreen)
+                
+            }
+            
+            else if operation == 13{ //Multiply
+                
+                label.text = String(previousNumber * numberOnScreen)
+                
+            }
+            
+            else if operation == 14{ //Subtract
+                
+                label.text = String(previousNumber - numberOnScreen)
+                
+            }
+            
+            else if operation == 15{ //Add
+                
+                label.text = String(previousNumber + numberOnScreen)
+                
+            }
+            
+        }
+        
+        else if sender.tag == 11{
+            
+            label.text = ""
+            
+            previousNumber = 0
+            
+            numberOnScreen = 0
+            
+            operation = 0
+            
+        }
+        
+        
+        
         
         
     }
